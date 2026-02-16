fa2c0b1abac338b1f28dcdcadf30a5034b7ce2ca
undertheseaaa <seamiranda5276@gmail.com>
2026-02-16 15:55:51 +0800

    chore(ui): remove Proposal and Features screens and navbar links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import Proposal from './screens/Proposal';
-import Features from './screens/Features';
 
 function App() {
   return (
@@ -17,8 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
-            <Route path="/proposal" element={<Proposal />} />
-            <Route path="/features" element={<Features />} />
+            
           </Routes>
         </Container>
       </main>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
@@ -11,12 +10,6 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <LinkContainer to="/proposal">
-                <Nav.Link>Proposal</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/features">
-                <Nav.Link>Features</Nav.Link>
-              </LinkContainer>
               <Nav.Link href="/cart">
                 <i className="fas fa-shopping-cart"></i> Cart
               </Nav.Link>
