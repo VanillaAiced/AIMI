57e827dbf5dc1bf518cdfa415be23eafdff8775b
undertheseaaa <seamiranda5276@gmail.com>
2026-02-16 15:13:57 +0800

    chore: remove unused imports from Footer to silence eslint

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
   // Footer text removed per request; layout left for designer to style.
