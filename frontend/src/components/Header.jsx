68f180df0f94f2431e43d00b4220cb0ea4ec3cde
undertheseaaa <seamiranda5276@gmail.com>
2026-02-16 16:01:17 +0800

    chore(ui): remove Cart link from Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,9 +10,6 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart"></i> Cart
-              </Nav.Link>
               <Nav.Link href="/login">
                 <i className="fas fa-user"></i> Sign In
               </Nav.Link>
