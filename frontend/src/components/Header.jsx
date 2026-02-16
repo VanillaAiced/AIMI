7cbe463d30db2a355b5522fbc62c9fb6920deb8a
undertheseaaa <seamiranda5276@gmail.com>
2026-02-16 16:08:51 +0800

    chore(auth): remove client-side sign-in/logout (delete LoginScreen, revert Header and App)

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Navbar, Container } from 'react-bootstrap';
 
-const Header = ({ user, onLogout }) => {
+const Header = () => {
   return (
     <header>
       <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">AIMI</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              {!user ? (
-                <Nav.Link as={Link} to="/login">
-                  <i className="fas fa-user"></i> Sign In
-                </Nav.Link>
-              ) : (
-                <>
-                  <Nav.Link as={Link} to="#">{user.name || user.email}</Nav.Link>
-                  <Nav.Link onClick={onLogout}>Logout</Nav.Link>
-                </>
-              )}
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
